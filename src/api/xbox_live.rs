48e6df9421e05d27aee07617773c15af5b589e7a
ExploitDemon <exploitdemon@gmail.com>
2023-06-21 00:56:42 -0700

    integrated futher error handling n/o env

--- src/api/xbox_live.rs
+++ src/api/xbox_live.rs
@@ -1,9 +1,11 @@
 // src/api/xbox_live.rs
-use crate::api::data::GamertagReservationResponse;
-use reqwest::{Client, header::HeaderMap};
-use std::error::Error;
 
-pub async fn reserve_gamertag(client: &Client, headers: HeaderMap) -> Result<GamertagReservationResponse, Box<dyn Error>> {
+use crate::api::data::GamertagReservationResponse;
+use crate::api::errors::GamertagError;
+use reqwest::{Client, header::HeaderMap, StatusCode};
+use std::env;
+
+pub async fn reserve_gamertag(client: &Client, headers: HeaderMap) -> Result<GamertagReservationResponse, GamertagError> {
     let url = "https://gamertag.xboxlive.com/gamertags/reserve";
 
     let body = r#"{
@@ -16,10 +18,16 @@ pub async fn reserve_gamertag(client: &Client, headers: HeaderMap) -> Result<Gam
         .headers(headers)
         .body(body.to_string())
         .send()
-        .await?;
+        .await
+        .map_err(|e| GamertagError::RequestError(e.to_string()))?;
 
-    let response_body: GamertagReservationResponse = res.json().await?;
-
-
-    Ok(response_body)
+    match res.status() {
+        StatusCode::OK => {
+            let response_body: GamertagReservationResponse = res.json().await.map_err(|e| GamertagError::RequestError(e.to_string()))?;
+            Ok(response_body)
+        },
+        status => {
+            Err(GamertagError::ApiCallFailed { status: status.as_u16(), message: res.text().await.unwrap_or_else(|_| String::new()) })
+        }
+    }
 }
